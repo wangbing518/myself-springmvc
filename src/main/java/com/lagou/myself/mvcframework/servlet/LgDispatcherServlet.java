ceb1aaa8cc9a953c71445bf1dd64ceaaf96776be
wangbing <13271010419>
2021-07-12 00:00:31 +0800

    feature(*): 初版

--- src/main/java/com/lagou/myself/mvcframework/servlet/LgDispatcherServlet.java
+++ src/main/java/com/lagou/myself/mvcframework/servlet/LgDispatcherServlet.java
@@ -1,5 +1,8 @@
 package com.lagou.myself.mvcframework.servlet;
 
+import com.lagou.myself.mvcframework.annotations.LagouController;
+import com.lagou.myself.mvcframework.annotations.LagouService;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.annotation.Annotation;
 import java.util.*;
 
 /**
@@ -101,7 +105,47 @@ public class LgDispatcherServlet extends HttpServlet {
      * 基于classNames缓存的类的全限定类名，以及反射技术，完成对象创建和管理
      */
     private void doInstance(){
-
+        if (classNames.size()==0){
+            return;
+        }
+        try {
+            for (int i=0;i<=classNames.size();i++){
+                String className = classNames.get(i);//com.lagou.demo.controller.DemoController
+                //反射
+                Class<?> aClass = Class.forName(className);
+                //区分controller，区分service
+                if (aClass.isAnnotationPresent(LagouController.class)){// A.isAnnotationPresent(B.class)；意思就是：注释B是否在此A上。如果在则返回true；不在则返回false。
+                    // controller的id此处不做过多处理，不取value了，就拿类的⾸字⺟⼩写作为id，保存到ioc中
+                    String simpleName = aClass.getSimpleName();
+                    String lowerFirstSimpleName = lowerFirst(simpleName);
+                    Object o = aClass.newInstance();//实例化这个对象
+                    ioc.put(lowerFirstSimpleName,o);
+                }else if (aClass.isAnnotationPresent(LagouService.class)){
+                    LagouService annotation = aClass.getAnnotation(LagouService.class);
+                    //获取注解value值
+                    String beanName = annotation.value();
+                    //如果指定了ID 就以指定的为准
+                    if (!"".endsWith(beanName.trim())){
+                        ioc.put(beanName,aClass.newInstance());
+                    }else {
+                        beanName=lowerFirst(aClass.getSimpleName());
+                        ioc.put(beanName,aClass.newInstance());
+                    }
+                    // service层往往是有接⼝的，⾯向接⼝开发，此时再以接⼝名为id，放⼊⼀份对象到ioc中，便于后期根据接⼝类型注⼊
+                    Class<?>[] interfaces = aClass.getInterfaces();
+                    for (int j=0;j<interfaces.length;j++){
+                        Class<?> anInterface = interfaces[j];
+                        // 以接⼝的全限定类名作为id放⼊
+                        String interfaceName = anInterface.getName();
+                        ioc.put(interfaceName,aClass.newInstance());
+                    }
+                }else {
+                    continue;
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     /**
